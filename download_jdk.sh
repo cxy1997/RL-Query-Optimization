c18255691649ce2af91002bff47e99a0b1a9ce72
Tsingularity <happyallen24@gmail.com>
2021-04-12 12:18:38 -0400

    add gdownload script for java

---
+++ download_jdk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. ./utils/gdownload.sh
+echo "downloading jdk..."
+gdownload 10ziyB7Vk3WYmv_OWR8KvhPClOe0tZsQ9 jdk.tar
+echo "jdk downloaded"
---
+++ download_jre.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. ./utils/gdownload.sh
+echo "downloading jre..."
+gdownload 16J7-fm_kmLlLgA5EMRZWnYcqES2aiNlR jre.tar
+echo "jre downloaded"
