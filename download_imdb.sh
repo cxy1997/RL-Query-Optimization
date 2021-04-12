8784ae9fc7b8fe2825a9f6ff0f3b4948891401c6
Tsingularity <happyallen24@gmail.com>
2021-04-12 13:49:18 -0400

    add download script for imdb

---
+++ download_imdb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. ./utils/gdownload.sh
+echo "downloading imdb..."
+gdownload 1Qhk-Mf2Otc6C9e-vIm2OQyKoko7kK_-- imdb_dump.sql
+echo "imdb downloaded"
--- download_jre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./utils/gdownload.sh
-echo "downloading jre..."
-gdownload 16J7-fm_kmLlLgA5EMRZWnYcqES2aiNlR jre.tar
-echo "jre downloaded"
